e23e46d2a29513e77fc4b06fa62697e247e41de2
Marouane28 <irhbiram@gmail.com>
2023-06-12 23:02:43 +0200

    ajout de la musique pour le jeu, pour la victoire et pour le gameover

--- src/main/java/com/application/saejeu/saejeu1/controleur/Controleur.java
+++ src/main/java/com/application/saejeu/saejeu1/controleur/Controleur.java
@@ -54,7 +54,7 @@ public class Controleur implements Initializable {
     private TileMap tileMap;
     @FXML
     private Label labelPieces, labelM, labelG, labelR;
-    private final int nb_manche = 10*; // permet de définir le nombre de manches
+    private final int nb_manche = 10; // permet de définir le nombre de manches
     private boolean estEnPause = false; // gerer les pauses
 
 
