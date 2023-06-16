9dd27d926545afa5b5985d1bc0002158112ed571
mirhbira <irhbiram@gmail.com>
2023-06-16 17:09:35 +0200

    version avec projectiles

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,15 +4,11 @@ module com.application.saejeu.saejeu1 {
     requires java.desktop;
 
     requires org.controlsfx.controls;
-    requires junit;
-    requires org.testng;
+
 
     opens com.application.saejeu.saejeu1 to javafx.fxml;
-    opens com.application.saejeu.saejeu1.controleur to javafx.fxml;
-    opens com.application.saejeu.saejeu1.modele to junit;
-
     exports com.application.saejeu.saejeu1;
     exports com.application.saejeu.saejeu1.controleur;
-    opens com.application.saejeu.saejeu1.modele.JUnit to junit;
-    opens com.application.saejeu.saejeu1.modele.Projectile to junit;
+    opens com.application.saejeu.saejeu1.controleur to javafx.fxml;
+
 }
